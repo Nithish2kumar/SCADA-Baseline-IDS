af80687253795b8ca50f33a6f99ee12311f08947
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-06-09 20:59:21 +0530

    Complete Phase 3 Modbus analysis

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from scapy.all import *
 from assets import *
 from detector import detect
+from parser import parse
 
-packets = rdpcap("mobile.pcap")
+packets = rdpcap("test.pcap")
 
 for pkt in packets:
     discovering(pkt)
     detect(pkt)
+    parse(pkt)
 
 printAssest()
 
---
+++ parser.py
@@ -0,0 +1,34 @@
+from scapy.all import *
+
+modbus_func={
+    1:"Read coils",
+    2:"Read discrete inputs",
+    3:"Read holding registers",
+    4:"Read Input registers",
+    5:"Write single coil",
+    6:"Write single register",
+    15:"Write multiple coils",
+    16:"Write multiple registers"
+}
+READ_CODES = [1, 2, 3, 4]
+WRITE_CODES = [5, 6, 15, 16]
+def parse(pkt):
+    if TCP in pkt:
+        if pkt[TCP].sport==502 or pkt[TCP].dport==502:
+            if Raw in pkt:
+                paylod=bytes(pkt[Raw].load)
+                if len(paylod)>=8:
+                    funcCode=paylod[7]
+                    if funcCode>=128:
+                        actual_fc = funcCode-128
+                        print(f"Exception response: {actual_fc}")
+                    else:
+                        name=modbus_func.get(funcCode,"Unknown")
+                        if funcCode in READ_CODES:
+                            print(f"[READ] {name}")
+
+                        elif funcCode in WRITE_CODES:
+                            print(f"[WRITE] {name}")
+
+                        else:
+                            print(f"[UNKNOWN] FC={funcCode}")
