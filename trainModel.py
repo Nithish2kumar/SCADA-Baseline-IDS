43a5e3de45f01dbdbe6209d06c5fe3410bd69655
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-06-27 22:29:53 +0530

    Reconstruced the input

--- trainModel.py
+++ trainModel.py
@@ -1,16 +1,15 @@
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-
 #Fetch data
-df = pd.read_csv("data/sensor_data.csv")
-df = df.drop("timestamp", axis=1)
+import pandas as pd
+df=pd.read_csv("data/sensor_data.csv")
+df=df.drop("timestamp", axis=1)
 
 #Normalizing
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(df)
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler()
+scaled_data=scaler.fit_transform(df)
 
 #Sampling the data
+import numpy as np
 windowSize=10
 def createSeq(data,window):
     sequences=[]
@@ -20,6 +19,27 @@ def createSeq(data,window):
     return np.array(sequences)
 
 x=createSeq(scaled_data,windowSize)
-print(type(x))
-print(x.shape)
+
 #LSTM
+import torch
+import torch.nn as nn
+xTensor=torch.tensor(x,dtype=torch.float32)
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder=nn.LSTM(input_size=6,hidden_size=64,batch_first=True)
+        self.decoder=nn.LSTM(input_size=64,hidden_size=64,batch_first=True)
+        self.output_layer=nn.Linear(64,6)
+    def forward(self, x):
+        _,(hidden,cell)=self.encoder(x)
+        hidden=hidden.squeeze(0)
+        decoderInput=hidden.unsqueeze(1)
+        decoderInput=decoderInput.repeat(1,10,1)
+        decoderOutput,_=self.decoder(decoderInput)
+        reconstructed=self.output_layer(decoderOutput)
+        return reconstructed
+    
+model=TestModel()
+recon=model(xTensor)
+print(recon.shape)
